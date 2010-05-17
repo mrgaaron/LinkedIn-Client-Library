878d94923f78a490ec2bcda5f62919d0d1a99b1e
Aaron <abrenzel@millerresource.com>
2010-05-17 10:01:59 -0500

    added analysis directory

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='abrenzel@millerresource.com',
     url='12.236.169.60:4830/liclient',
     description='Library for accessing the LinkedIn API in Python',
-    packages=['liclient', 'liclient.parsers', 'liclient.oauth'],
+    packages=['liclient', 'liclient.parsers', 'liclient.oauth', 'liclient.analysis'],
     license='MIT',
     package_data='*.txt'
 )
